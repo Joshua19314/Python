9d109722518d2bcdbd8e29be80e9c66edc89411c
Joshua19314 <josh1.adeleye@gmail.com>
2026-02-15 21:53:54 +0000

    Add files via upload

---
+++ acp.py
@@ -0,0 +1,10 @@
+John_birthday = "1-1-2000"
+print("John's birthday is :", John_birthday)
+Kate_birthday = "2-2-2001"
+print("Kate's birthday is :", Kate_birthday)
+Devonte_birthday = "3-3-2002"
+print("Devonte's birthday is :", Devonte_birthday)
+Joel_birthday = "4-4-2003"
+print("Joel's birthday is :", Joel_birthday)
+Emma_birthday = "5-5-2004"
+print("Emma's birthday is :", Emma_birthday)
